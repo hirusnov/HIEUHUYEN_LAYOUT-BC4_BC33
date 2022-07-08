93e8d6a9bfc6f39b469aef25b78417b1eaacf62e
hieu.tb <hirusmeo@gmail.com>
2022-07-08 20:50:04 +0700

    Final Home page (Not responsive)

---
+++ js/owlCarousel.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            768: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: false,
+                loop: false
+            }
+        }
+    })
+})

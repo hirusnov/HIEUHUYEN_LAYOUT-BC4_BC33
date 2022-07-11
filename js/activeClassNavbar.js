bc823105359bb7fd5715ba9bc945701f0b62b132
hieu.tb <hirusmeo@gmail.com>
2022-07-12 00:58:21 +0700

    Add BLog page UI

--- js/activeClassNavbar.js
+++ js/activeClassNavbar.js
@@ -1,5 +1,8 @@
 if ((location.pathname.split("/")[1]) !== ""){
     $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+    if ($('.dropdown-item').hasClass('active')) {
+        $('#navbarDropdown').addClass('active');
+    }
 } else {
-$('nav #home').addClass('active');
+    $('nav #home').addClass('active');
 }
